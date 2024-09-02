7cbd23809cc894c5095d35ddbf28907644b6dd41
Tony <fedosov-toxa@mail.ru>
2024-09-02 23:29:31 +0300

    configure put|get tftd server-commands on windows

--- client/client.py
+++ client/client.py
@@ -7,30 +7,42 @@ from GUI.client import Ui_MainWindow
 class MainClass(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-
+        self.file_name = None
+        self.change_size(341, 300)
         self.setupUi(self)
         self.setWindowTitle('Клиент тестирования')
         self.pushButton_2.clicked.connect(lambda: self.choose_operator(page_index=1))
         self.pushButton.clicked.connect(lambda: self.choose_operator(page_index=2))
-        # self.pushButton_2.clicked.connect(lambda: self.choose_operator(page_index=1, button=self.pushButton_2))
-        # self.pushButton_3.clicked.connect(lambda: self.choose_operator(page_index=1, button=self.pushButton_3))
-        # self.pushButton_4.clicked.connect(lambda: self.choose_operator(page_index=1, button=self.pushButton_4))
-        # self.pushButton_5.clicked.connect(self.get_new_task)
 
     def choose_operator(self, page_index):
-        self.stackedWidget.setCurrentIndex(page_index)
-        if page_index == 1:
-            self.setFixedWidth(351)
-            self.setFixedHeight(531)
+        if page_index == 1 and self.lineEdit.text() != "":
+            self.change_size(351, 531)
+            self.stackedWidget.setCurrentIndex(page_index)
+        elif self.lineEdit.text() == "":
+            err_dialog = QtWidgets.QErrorMessage(self)
+            err_dialog.showMessage("Заполните поле ФИО!")
         elif page_index == 2:
-            self.setFixedWidth(721)
-            self.setFixedHeight(531)
+            self.change_size(721, 531)
+            self.file_name = '_'.join(self.lineEdit.text().split())
+            pathlib.Path(f'{self.file_name}.txt').touch()
+            pathlib.Path(f'{self.file_name}.txt').write_text(self.textEdit.toPlainText())
+            os.system(f'tftp 127.0.0.1 PUT {self.file_name}.txt')
+            pathlib.Path(f'{self.file_name}.txt').unlink()
+            self.stackedWidget.setCurrentIndex(page_index)
+            os.system(f'tftp 127.0.0.1 GET {self.file_name}.txt')
+            with open(f'{self.file_name}.txt', 'r') as f:
+                text = f.read()
+            self.textEdit_2.setText(text)
+            file_name_etalon = self.lineEdit_4.text()
+            os.system(f'tftp 127.0.0.1 GET {file_name_etalon}')
+            with open(f'{file_name_etalon}', 'r') as f_etalon:
+                et_text = f_etalon.read()
+                print(et_text)
+            self.textEdit_3.setText(et_text)
 
-    def put_file(self):
-        pathlib.Path('test.txt').touch()
-        pathlib.Path('test.txt').write_text('test')
-        os.system(f'tftp 127.0.0.1 PUT {"test.txt"}')
-        # pathlib.Path('test.txt').unlink()
+    def change_size(self, width, height):
+        self.setFixedWidth(width)
+        self.setFixedHeight(height)
 
 
 if __name__ == "__main__":
