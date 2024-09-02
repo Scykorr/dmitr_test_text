0f23a43d6052b7173abfedb035282102508690b8
Tony <fedosov-toxa@mail.ru>
2024-09-02 22:37:28 +0300

    put func for windows done && button get next page

--- client/client.py
+++ client/client.py
@@ -1,3 +1,5 @@
+import pathlib
+import os
 from PyQt5 import QtCore, QtGui, QtWidgets
 from GUI.client import Ui_MainWindow
 
@@ -8,17 +10,27 @@ class MainClass(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.setupUi(self)
         self.setWindowTitle('Клиент тестирования')
-        self.puss
-        # self.pushButton.clicked.connect(lambda: self.choose_operator(page_index=1, button=self.pushButton))
+        self.pushButton_2.clicked.connect(lambda: self.choose_operator(page_index=1))
+        self.pushButton.clicked.connect(lambda: self.choose_operator(page_index=2))
         # self.pushButton_2.clicked.connect(lambda: self.choose_operator(page_index=1, button=self.pushButton_2))
         # self.pushButton_3.clicked.connect(lambda: self.choose_operator(page_index=1, button=self.pushButton_3))
         # self.pushButton_4.clicked.connect(lambda: self.choose_operator(page_index=1, button=self.pushButton_4))
         # self.pushButton_5.clicked.connect(self.get_new_task)
 
-    def choose_operator(self, page_index, button):
+    def choose_operator(self, page_index):
         self.stackedWidget.setCurrentIndex(page_index)
-        self.textBrowser.append(button.text())
+        if page_index == 1:
+            self.setFixedWidth(351)
+            self.setFixedHeight(531)
+        elif page_index == 2:
+            self.setFixedWidth(721)
+            self.setFixedHeight(531)
 
+    def put_file(self):
+        pathlib.Path('test.txt').touch()
+        pathlib.Path('test.txt').write_text('test')
+        os.system(f'tftp 127.0.0.1 PUT {"test.txt"}')
+        # pathlib.Path('test.txt').unlink()
 
 
 if __name__ == "__main__":
